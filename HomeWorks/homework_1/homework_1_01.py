18f54d761d0dfd7eb730325f44672a5a6de14e7a
Ольга Саутина <ogonek_kenogo@mail.ru>
2021-10-19 20:35:54 +0300

    1е домашнее задание

---
+++ HomeWorks/homework_1/homework_1_01.py
@@ -0,0 +1,27 @@
+#Работа с числами
+a = 10
+print(f'a={a}')
+
+b = 15.5
+print(f'b={b}')
+
+print(f'Сумма: {a + b}')
+print(f'Разность: {a - b}')
+print(f'Произведение: {a * b}')
+print(f'Частное: {a / b}')
+
+#Работа со строками
+name = input('Как вас зовут? ')
+year = input('Сколько вам лет? ')
+town = input('В каком городе вы живете? ')
+print('Итак, вы указали следующие данные:')
+print(f'Вас зовут {name}')
+print(f'Вам {year} лет(год)')
+print(f'Вы живете в городе {town}')
+
+#Работа со списком
+fruits_list = ['apple', 'orange', 'melon']
+print(fruits_list)
+print(fruits_list[0])
+print(fruits_list[1])
+print(fruits_list[2])
